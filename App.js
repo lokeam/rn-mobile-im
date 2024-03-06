8cd51400ebc09b5c52927dfe35f47372c44ea352
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-06 01:48:56 -0500

    feat: add custom splash screen + assets

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
+import { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          'black': require('./assets/fonts//Roboto-Black.ttf'),
+          'blackItalic': require('./assets/fonts/Roboto-BlackItalic.ttf'),
+          'bold': require('./assets/fonts/Roboto-Bold.ttf'),
+          'boldItalic': require('./assets/fonts/Roboto-BoldItalic.ttf'),
+          'italic': require('./assets/fonts/Roboto-Italic.ttf'),
+          'light': require('./assets/fonts/Roboto-Light.ttf'),
+          'lightItalic': require('./assets/fonts/Roboto-LightItalic.ttf'),
+          'medium': require('./assets/fonts/Roboto-Medium.ttf'),
+          'mediumItalic': require('./assets/fonts/Roboto-MediumItalic.ttf'),
+          'regular': require('./assets/fonts/Roboto-Regular.ttf'),
+          'thin': require('./assets/fonts/Roboto-Thin.ttf'),
+          'thinItalic': require('./assets/fonts/Roboto-ThinItalic.ttf'),
+        });
+      } catch(error) {
+        console.error('I am error. Problem loading fonts');
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    loadFonts();
+  }, []);
+
+
+  const onLayout = useCallback(
+    async () => {
+      if (appIsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider  style={styles.container}>
+    <SafeAreaProvider
+      style={styles.container}
+      onLayout={onLayout}
+    >
       <SafeAreaView>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>React Native Mobile Instant Messenger</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -19,4 +68,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: "regular"
+  }
 });
