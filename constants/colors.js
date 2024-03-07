8bfcafc17771777a7e7f88d4b0f0491d34e7da5d
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-07 14:35:26 -0500

    style: adding styles to Message Screen message area, input + icons

---
+++ constants/colors.js
@@ -0,0 +1,26 @@
+// Todo: separate these values out into design-system color pallette colros
+export default {
+  darkTheme: {
+    borderUIColor: '#d1d7db',
+    headerFooterBackground: '#202c33', //32,44,51
+    iconSVGFillColor: '#8696a0', //134, 150, 160
+    inputBackground: '#2a3942',
+    inputTextColor: '#d1d7db', //209, 215, 219
+    inputPlaceholderTextColor: '#d1d7db',
+    messageDateLabel: '##8696a0', //134, 150, 160
+    messageTextColor: '#e9edef', // 233, 237, 239
+    messageTimestampColor: 'rgba(255, 255, 255, 0.6)',
+    messageLinkColor: '#53bdeb', //83, 189, 235
+    messageSVGFill: 'rgba(233, 237, 239, 0.5)',
+    messagerBubbleBackground: '#005c4b', // 0, 92, 75,
+    profileSubTitleColor: '#aebac1', // 174, 186, 193
+    settingsMenuBackground: '#101a20', //16, 26, 32
+    settingsMenuIconFill: '#d1d7db', //209, 215, 219
+    warningColor: '#f15c6d' //241, 92, 109
+  },
+  headerFooterBackground: '#202c33', //32,44,51
+  inputBackground: '#2a3942',
+  inputTextColor: '#d1d7db',
+  messageTextColor: '#e9edef',
+  messageSendButtonColor: '#0e9674'
+}
--- screens/MsgScreen.js
+++ screens/MsgScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   ImageBackground,
@@ -9,10 +9,12 @@ import {
  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import backgroundImage from '../assets/images/bg_MsgScreenDark.jpeg';
-import { Feather } from '@expo/vector-icons';
+import colors from '../constants/colors';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const MsgScreen = (props) => {
+  const [messageText, setMessageText] = useState('');
 
     return (
       <SafeAreaView
@@ -24,19 +26,36 @@ const MsgScreen = (props) => {
           style={styles.backgroundImage}
         >
         </ImageBackground>
-        <View style={styles.inputContainer}>
-          <TouchableOpacity onPress={() => console.log('add media pressed')}>
-            <Feather name="paperclip" size={24} color="black" />
-          </TouchableOpacity>
-          <TextInput />
-          <TouchableOpacity onPress={() => console.log('camera pressed')}>
-            <MaterialIcons name="camera-alt" size={24} color="black" />
-          </TouchableOpacity>
+        <View style={styles.footerWrapper}>
+          <View style={styles.inputContainer}>
+            <TouchableOpacity onPress={() => console.log('add media pressed')}>
+              <FontAwesome5 name="plus" size={24} color="#8696a0" style={styles.footerIcon} />
+            </TouchableOpacity>
+
+            <TextInput
+              onChangeText={text => setMessageText(text)}
+              placeholder="Message"
+              placeholderTextColor="#d1d7db"
+              style={styles.textBox}
+              value={messageText}
+            />
+            <TouchableOpacity
+              onPress={() => console.log('camera pressed')}
+              style={{ ...styles.mediaButton, ...styles.sendButton }}
+            >
+              { messageText === "" ? (
+                <MaterialIcons name="camera-alt" size={28} color="#d1d7db" style={styles.footerIconCamera} />
+              ) : (
+                <MaterialIcons name="send" size={25} color="#d1d7db" style={styles.footerIconCamera} />
+              )}
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     )
 };
 
+// To Research: Unistyles (https://reactnativeunistyles.vercel.app/)
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1
@@ -47,8 +66,38 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flexDirection: 'row',
+    paddingVertical: 9,
+    paddingHorizontal: 12
+  },
+  footerIcon: {
     paddingVertical: 8,
-    paddingHorizontal: 10
+    paddingHorizontal: 8
+  },
+  footerIconCamera: {
+  },
+  footerWrapper: {
+    backgroundColor: colors.headerFooterBackground,
+    paddingVertical: 5,
+  },
+  mediaButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 44,
+  },
+  sendButton: {
+    backgroundColor: colors.messageSendButtonColor,
+    borderRadius: 50
+  },
+  textBox: {
+    backgroundColor: colors.inputBackground,
+    borderColor: colors.inputBackground,
+    color: colors.messageTextColor,
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingVertical: 9,
+    paddingHorizontal: 12,
+    marginHorizontal: 8
   }
 })
 
