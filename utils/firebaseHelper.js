ec94f9da501992a1dd6c54969dcd9b1c9f97fc23
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-12 23:24:41 -0400

    build: add Firebase + helper util

---
+++ utils/firebaseHelper.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+export const getFirebaseApp = () => {
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MSG_SENDER_ID,
+    appId: process.env.FB_APP_ID,
+    measurementId: process.env.FB_MEASUREMENT_ID
+  };
+
+  // Initialize Firebase
+  return initializeApp(firebaseConfig);
+};
