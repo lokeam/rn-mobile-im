454c43ffd094701b95ad7cb61a6c9801ea4f15a1
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-13 22:04:44 -0400

    feat: add redux toolkit, configure auth slice

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 LogBox.ignoreLogs(['Async storage has been extracted']);
 
@@ -55,9 +57,12 @@ export default function App() {
   }
 
   return (
-    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
-      <AppNavigator />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+        <AppNavigator />
+      </SafeAreaProvider>
+    </Provider>
+
   );
 }
 
